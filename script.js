62a1a36fdfd2041405ae6c0a2fece1cabf8d9824
CC9898A <98588553+CC9898A@users.noreply.github.com>
2022-05-14 17:50:14 +0200

    Add files via upload
    Triangle app

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//area 
+function area() {
+let h=document.getElementById("altezza").value;
+let b=document.getElementById("base").value;
+let complete="l'area è";
+let a= h*b/2;
+
+console.log(complete+" " ,a)
+}
+
+//altezza
+function two() {
+    let height=document.getElementById("ares").value;
+    let one=document.getElementById("basex").value;
+    let final=height*2/one;
+    console.log(final)
+}
+//base
+function three(){
+let alt=document.getElementById("alt").value;
+let are=document.getElementById("ar").value;
+
+let finale=are*2/alt
+console.log(finale)
+}
